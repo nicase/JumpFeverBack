5e4a32a998618dc6662c7903685683212d07fd64
Pau Escofet <pau.escofet@est.fib.upc.edu>
2020-03-21 11:44:25 +0100

    login

--- src/static/js/login.js
+++ src/static/js/login.js
@@ -1,51 +1,62 @@
 function post(url, load, callback) {
-  var req = new XMLHttpRequest();   // new HttpRequest instance
-  req.open("POST", url);
-  req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  const req = new XMLHttpRequest(); // new HttpRequest instance
+  req.open('POST', url);
+  req.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
   req.onreadystatechange = () => {
     if (req.readyState == XMLHttpRequest.DONE) {
-      callback(req.responseText)
+      callback(req.responseText);
     }
-  }
+  };
   req.send(JSON.stringify(load));
 }
 
 function error() {
-  document.getElementById("hello").innerHTML = "Something went wrong!";
+  document.getElementById('hello').innerHTML = 'Something went wrong!';
 }
 
 function save_data(resp) {
   try {
     resp = JSON.parse(resp);
-  }
-  catch (err) {
-    error()
+  } catch (err) {
+    error();
   }
   // Login correcte
   if (resp.token) {
-    const username = document.getElementById("usernameInput").value;
+    const username = document.getElementById('usernameInput').value;
     localStorage.setItem('token', resp.token);
     localStorage.setItem('username', username);
     // Redirect
-    document.cookie = "token=" + resp.token;
-    window.location.replace("/dashboard");
+    document.cookie = `token=${resp.token}`;
+    window.location.replace('/dashboard');
   }
   // Login incorrecte
   else {
-    error()
+    error();
   }
 }
 
 function try_login() {
+  const username = document.getElementById('usernameInput').value;
+  const password = document.getElementById('passwordInput').value;
 
-  const username = document.getElementById("usernameInput").value;
-  const password = document.getElementById("passwordInput").value;
+  const load = {
+    username,
+    password,
+  };
 
-  var load = {
-    "username": username,
-    "password": password
-  }
+  post('/api/auth/login', load, save_data);
+}
 
-  post("/api/auth/login", load, save_data)
+function try_signup() {
+  const username = document.getElementById('usernameInput').value;
+  const email = document.getElementById('emailInput').value;
+  const password = document.getElementById('passwordInput').value;
 
+  const load = {
+    username,
+    email,
+    password,
+  };
+
+  post('/api/auth/signup', load, save_data);
 }
