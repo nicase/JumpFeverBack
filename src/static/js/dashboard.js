f793d009d09dfb23774cd8c4300f83a9896602ae
Nico <nicase99@gmail.com>
2020-04-01 12:23:30 +0200

    meihjd

---
+++ src/static/js/dashboard.js
@@ -0,0 +1,27 @@
+function get(url, load, callback) {
+  var req = new XMLHttpRequest();   // new HttpRequest instance
+  req.open("POST", url);
+  req.setRequestHeader("Content-Type", "application/json");
+  let tokenHeader = 'Bearer ' + localStorage.getItem('token');
+  req.setRequestHeader('Authorization', tokenHeader);
+  req.onreadystatechange = () => {
+    if (req.readyState == XMLHttpRequest.DONE) {
+      callback(req.responseText)
+    }
+  }
+  req.send(JSON.stringify(load));
+}
+
+function saveMatchID(matchID) {
+    localStorage.setItem("matchID", matchID);
+}
+
+function joinGame() {
+
+  var matchID = document.getElementById("code").value
+  saveMatchID(matchID)
+  var url = "/api/match/" + matchID;
+  //console.log(url)
+  window.location.replace(url);
+
+}
